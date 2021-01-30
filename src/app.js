577e0a0d54b161c5519fec8c2e72f54e441195e4
igorcasconi <igor492@gmail.com>
2021-01-30 15:09:20 -0300

    feat(master): added initial project config

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(4000, () => {
+  console.log('Aplicação rodando na porta 4000')
+});
---
+++ src/database/connection.js
@@ -0,0 +1,10 @@
+var knex = require('knex')({
+  client: 'mysql',
+  connection: {
+      host : process.env.DATABASE_URL,
+      user : process.env.DATABASE_USER,
+      password : process.env.DATABASE_PASS,
+      database : process.env.DATABASE_NAME 
+   }
+});
+module.exports = knex
