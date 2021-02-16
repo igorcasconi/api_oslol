2a7288e979519f1787cce06784b5a3c747b6a79b
igorcasconi <igor492@gmail.com>
2021-02-16 10:49:03 -0300

    fix(login): fixed validation error

--- src/controllers/data-control-panel/LoginController.js
+++ src/controllers/data-control-panel/LoginController.js
@@ -2,16 +2,17 @@ import jwt from 'jsonwebtoken'
 import md5 from 'blueimp-md5'
 
 import db from '../../database/connection'
-
-
 export default class LoginController {
 
   async authenticate (req, res) {
 
+    if (!req.body.email || !req.body.password) return res.status(400).json({error: 'Usuário ou senha está vazio!'});
+
     const email = req.body.email;
     const pass = md5(req.body.password);
 
-    const query = await db('fl_users').where('users_email', '=', email)
+    const query = await db('fl_users')
+    .where('users_email', '=', email)
     .where('users_pass', '=', pass).select('*').limit(1);
 
     if (query == '') return res.status(400).json({error: 'Usuário ou senha incorreto!'});
