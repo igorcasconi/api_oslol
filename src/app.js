3f3fb50115e124eaa55f312c6a9c674fa92715ad
Igor Casconi de Oliveira <igor.oliveira@nav9.tech>
2023-03-01 11:22:10 -0300

    fix(app): code eslint fixed

--- src/app.js
+++ src/app.js
@@ -1,24 +1,26 @@
-import dotenv from 'dotenv'
-import express from 'express';
-import cors from 'cors'
-import bodyParser from 'body-parser'
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
 
-import routes from './routes';
+import routes from "./routes";
 
-dotenv.config({path: '../.env'});
+dotenv.config({ path: "../.env" });
 const app = express();
-const port = process.env.PORT || 5000
-const host = '0.0.0.0';
+const port = process.env.PORT || 5000;
+const host = "0.0.0.0";
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.set('keySecret', process.env.PRIVATE_KEY);
+app.set("keySecret", process.env.PRIVATE_KEY);
 
 app.listen(port, host, () => {
-  console.log('Aplicação rodando na porta 5000')
+  console.log(`Aplicação rodando na porta ${port}`);
 });
